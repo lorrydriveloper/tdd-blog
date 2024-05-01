df0f1528e507bd5ffacbfacd451abbd4ecd79837
lorrydriveloper <lorrydriveloper@gmail.com>
2024-05-01 08:57:58 +0100

    Add Capybara and Webdrivers gems for testing

--- Gemfile
+++ Gemfile
@@ -56,6 +56,11 @@ group :development, :test do
   gem "shoulda-matchers"
 end
 
+group :test do
+  gem "capybara"
+  gem "webdrivers"
+end
+
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,12 @@ RSpec.configure do |config|
     Rails.root.join("spec/fixtures")
   ]
 
+  config.before(:each, type: :system) do
+    driver = :selenium_chrome_headless
+    driver = :selenium_chrome if ENV["SHOW_BROWSER"]
+    driven_by(driver)
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
